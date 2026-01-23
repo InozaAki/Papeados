cd15767b7d6425a98d2ce34fee9cdaaf3d0b319d
InozaAki <axelespinoza887@gmail.com>
2026-01-23 10:52:34 -0600

    feat: Update player.gd to add fall and idle animations

--- papeados/Assets/Scripts/player.gd
+++ papeados/Assets/Scripts/player.gd
@@ -12,6 +12,8 @@ const FRICTION = 600
 @export var dash_duration := 0.2
 @export var dash_cooldown := 1.5
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var can_double_jump := true
 var can_dash := true
 var is_dashing := false
@@ -21,26 +23,40 @@ func _physics_process(delta: float) -> void:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var current_speed = dash_speed if is_dashing else SPEED
 	
+
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		velocity.y = min(velocity.y, MAX_FALL_SPEED)
+
+		if is_dashing:
+			velocity.x = move_toward(velocity.x, input_vector.x * dash_speed, ACCELERATION * delta)
+		
+		if velocity.y > 0:
+			animated_sprite.play("fall") 
+
 	else:
 		can_double_jump = true
 		velocity.y = 0
-		
+
 	if input_vector.x != 0:
 		velocity.x = move_toward(velocity.x, input_vector.x * current_speed, ACCELERATION * delta)
+		animated_sprite.flip_h = velocity.x < 0
+		animated_sprite.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		if is_on_floor():
+			animated_sprite.play("idle")
 	
 	# Jumping
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
 
 	# Double jump
 	if Input.is_action_just_pressed("ui_up") and not is_on_floor() and velocity.y > 0 and can_double_jump:
 		can_double_jump = false
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
 
 	# Dashing
 	if Input.is_action_just_pressed("dash") and can_dash and input_vector.length() > 0:
