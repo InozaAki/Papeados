cbcf84cb55b883d5c6860b5c450264c9ad448fb2
Bjma1507 <123825171+Bjma1507@users.noreply.github.com>
2026-04-10 20:59:18 -0600

    feat: Implementacion de fondo en world

--- papeados/ui/menus/main_menu.gd
+++ papeados/ui/menus/main_menu.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var crear_partida_btn = $CrearPartidaBtn
 @onready var unirse_btn = $UnirseBtn
 @onready var salir_btn = $SalirBtn
+@onready var network_manager = get_node("/root/NetworkManager")
 
 func _ready():
 	crear_partida_btn.pressed.connect(_on_crear_pressed)
@@ -10,9 +11,11 @@ func _ready():
 	salir_btn.pressed.connect(_on_salir_pressed)
 
 func _on_crear_pressed():
+	network_manager.set_meta("modo_host", true)
 	get_tree().change_scene_to_file("res://multiplayer/lobby/lobby.tscn")
 
 func _on_unirse_pressed():
+	network_manager.set_meta("modo_host", false)
 	get_tree().change_scene_to_file("res://multiplayer/lobby/lobby.tscn")
 
 func _on_salir_pressed():
--- papeados/world/world_bounds.gd
+++ papeados/world/world_bounds.gd
@@ -9,16 +9,16 @@ func _ready() -> void:
 	create_walls()
 
 func create_walls():
-	# Floor
+	# Floor - Alargado y centrado
 	create_wall(
 		Vector2(0, arena_height / 2),
-		Vector2(arena_width, boundary_thickness)
+		Vector2(arena_width + boundary_thickness, boundary_thickness)
 	)
 
-	# Top
+	# Top - Alargado y centrado
 	create_wall(
 		Vector2(0, -arena_height / 2),
-		Vector2(arena_width, boundary_thickness)
+		Vector2(arena_width + boundary_thickness, boundary_thickness)
 	)
 
 	# Left
@@ -33,7 +33,6 @@ func create_walls():
 		Vector2(boundary_thickness, arena_height)
 	)
 
-
 func create_wall(pos: Vector2, size: Vector2) -> void:
 	var wall := StaticBody2D.new()
 	var collision_shape := CollisionShape2D.new()
@@ -43,3 +42,16 @@ func create_wall(pos: Vector2, size: Vector2) -> void:
 	wall.position = pos
 	wall.add_child(collision_shape)
 	add_child(wall)
+
+	var visual = Panel.new()
+	var sb = StyleBoxFlat.new()
+	sb.bg_color = Color(0.15, 0.15, 0.15, 1.0) 
+	sb.border_color = Color(1.0, 0.0, 1.0, 1.0) 
+	sb.border_width_left = 4
+	sb.border_width_top = 4
+	sb.border_width_right = 4
+	sb.border_width_bottom = 4
+	visual.add_theme_stylebox_override("panel", sb)
+	visual.size = size 
+	visual.position = -size / 2
+	wall.add_child(visual)
